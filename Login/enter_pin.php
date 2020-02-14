a9dfbe26770a6fc3d27694558f621fe669b3af7a
h81183gb <gurneet.bhatia@student.manchester.ac.uk>
2020-02-14 13:44:19 +0000

    closes pin modal when needed

--- Login/enter_pin.php
+++ Login/enter_pin.php
@@ -15,7 +15,7 @@ include('login.html');
   <!-- Modal content -->
   <form class="modal-form">
 	  <div class="modal-content">
-	    <span class="close">&times;</span>
+	    <span id="close" class="close">&times;</span>
 	    <h2>Enter Pin</h2>
 	    <label for="email"><b>Email</b></label>
 	    <input type="text" placeholder="Enter Email" name="email" required>
@@ -26,17 +26,16 @@ include('login.html');
 	</form>
 </div>
 
-<!--<div class="form-popup" id="myForm">
-  <form action="/action_page.php" class="form-container">
-    <h1>Login</h1>
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
-
-    <label for="psw"><b>Pin</b></label>
-    <input type="password" placeholder="Enter Pin" name="psw" required>
-
-    <button type="submit" class="btn">Enter</button>
-    <button type="submit" class="btn cancel" onclick="closeForm()">Close</button>
-  </form>
-</div>-->
+<script type="text/javascript">
+	var modal = document.getElementById("myModal");
+	var close = document.getElementById("close");
+	console.log(modal);
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			window.location = "login.php";
+		}
+		else if (event.target == close) {
+			window.location = "login.php";
+		}
+	}
+</script>>
